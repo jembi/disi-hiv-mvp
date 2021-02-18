045ec8cd81a0f3caa9159e17c9e6d40f94e260d2
Ryan Crichton <ryan@jembi.org>
2021-02-18 10:08:09 +0200

    Correct compose project so that service can see eachother

--- packages/data-analytics/docker/compose.sh
+++ packages/data-analytics/docker/compose.sh
@@ -3,13 +3,13 @@
 composeFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 if [ "$1" == "init" ]; then
-    docker-compose -p healthworker -f "$composeFilePath"/docker-compose.elastic.yml up -d
+    docker-compose -p instant -f "$composeFilePath"/docker-compose.elastic.yml up -d
 elif [ "$1" == "up" ]; then
-    docker-compose -p healthworker -f "$composeFilePath"/docker-compose.elastic.yml up -d
+    docker-compose -p instant -f "$composeFilePath"/docker-compose.elastic.yml up -d
 elif [ "$1" == "down" ]; then
-    docker-compose -p healthworker -f "$composeFilePath"/docker-compose.elastic.yml stop
+    docker-compose -p instant -f "$composeFilePath"/docker-compose.elastic.yml stop
 elif [ "$1" == "destroy" ]; then
-    docker-compose -p healthworker -f "$composeFilePath"/docker-compose.elastic.yml down -v
+    docker-compose -p instant -f "$composeFilePath"/docker-compose.elastic.yml down -v
 else
     echo "Valid options are: init, up, down, or destroy"
 fi
