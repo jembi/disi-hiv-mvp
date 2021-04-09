6469c6ab03dda24878d894a235fb6a992d6aa4ef
Ryan Crichton <ryan@jembi.org>
2021-04-09 13:13:20 +0200

    Add logstash example pipeline

---
+++ fhir-extrator/index.mjs
@@ -0,0 +1,53 @@
+import express from 'express'
+import fetch from 'node-fetch'
+import asyncHandler from 'express-async-handler'
+import fs from 'fs'
+
+const PORT = process.env.PORT || 3000
+const GET_URL = process.env.GET_URL || 'http://localhost:3447/fhir'
+const POST_URL = process.env.POST_URL || 'http://localhost:5055/fhir'
+const SYNC_FILE = 'lastSync.txt'
+
+const app = express()
+app.use(express.json({ type: 'application/fhir+json' }))
+
+app.get(
+  '/fhir-extract',
+  asyncHandler(async (req, res) => {
+    let ts
+    try {
+      ts = fs.readFileSync(SYNC_FILE).toString()
+    } catch (err) {
+      ts = new Date(0).toISOString()
+      fs.writeFileSync(SYNC_FILE, ts)
+    }
+
+    const bundleResponse = await fetch(`${GET_URL}/_history?_since=${ts}`, {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/fhir+json'
+      }
+    })
+
+    const newTS = new Date().toISOString()
+    const submitResponse = await fetch(POST_URL, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/fhir+json'
+      },
+      body: bundleResponse.body
+    })
+
+    fs.writeFileSync(SYNC_FILE, newTS)
+
+    if (submitResponse.ok) {
+      res.sendStatus(200)
+    } else {
+      res.sendStatus(submitResponse.size)
+    }
+  })
+)
+
+app.listen(PORT, () => {
+  console.log(`FHIR Extractor listening at http://localhost:${PORT}`)
+})
--- packages/data-pipeline/docker/compose.sh
+++ packages/data-pipeline/docker/compose.sh
@@ -3,9 +3,9 @@
 composeFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 if [ "$1" == "init" ]; then
-    docker create --name spark-helper -v spark-deps:/usr/local/spark-2.4.5-bin-hadoop2.7/jars jupyter/all-spark-notebook:spark-2
-    docker cp "$composeFilePath"/elasticsearch-spark-20_2.11-7.12.0.jar spark-helper:/usr/local/spark-2.4.5-bin-hadoop2.7/jars/
-    docker rm spark-helper
+    docker create --name logstash-helper -v logstash-data:/pipeline/ busybox
+    docker cp "$composeFilePath"/fhir-extractor.conf logstash-helper:/pipeline/
+    docker rm logstash-helper
 
     docker-compose -p instant -f "$composeFilePath"/docker-compose.yml up -d
 elif [ "$1" == "up" ]; then
@@ -14,7 +14,6 @@ elif [ "$1" == "down" ]; then
     docker-compose -p instant -f "$composeFilePath"/docker-compose.yml stop
 elif [ "$1" == "destroy" ]; then
     docker-compose -p instant -f "$composeFilePath"/docker-compose.yml down -v
-    docker volume rm spark-deps
 else
     echo "Valid options are: init, up, down, or destroy"
 fi
