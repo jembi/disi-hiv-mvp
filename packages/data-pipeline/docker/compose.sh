ab899800e225813d6e62c65a3cf74f25b78ae76f
Ryan Crichton <ryan@jembi.org>
2021-04-13 16:14:19 +0200

    Add multiple pipeline processing
    One pipeline for storing raw events and one for enriching the events with more information for reporting. The enrich pipeline queries data from the raw events index to add more details. This is just a simple first example.

--- packages/data-pipeline/docker/compose.sh
+++ packages/data-pipeline/docker/compose.sh
@@ -3,8 +3,14 @@
 composeFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 if [ "$1" == "init" ]; then
-    docker create --name logstash-helper -v logstash-data:/pipeline/ busybox
+    docker create --name logstash-helper -v logstash-pipeline:/pipeline/ -v logstash-config:/config/ busybox
     docker cp "$composeFilePath"/fhir-extractor.conf logstash-helper:/pipeline/
+    docker cp "$composeFilePath"/fhir-enrich.conf logstash-helper:/pipeline/
+    docker cp "$composeFilePath"/id-query.json logstash-helper:/pipeline/
+    docker cp "$composeFilePath"/jvm.options logstash-helper:/config/
+    docker cp "$composeFilePath"/log4j2.properties logstash-helper:/config/
+    docker cp "$composeFilePath"/logstash.yml logstash-helper:/config/
+    docker cp "$composeFilePath"/pipelines.yml logstash-helper:/config/
     docker rm logstash-helper
 
     docker-compose -p instant -f "$composeFilePath"/docker-compose.yml up -d
